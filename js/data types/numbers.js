83e9b9c94cb40aa46f074f58090319b8d866f451
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2022-08-05 20:40:21 -0500

    Create numbers.js

---
+++ js/data types/numbers.js
@@ -0,0 +1,31 @@
+// Number
+// The number type represents both integer and floating point numbers.
+let n = 123;
+n = 12.345;
+
+// Number special numeric values
+console.log(1 / 0); // Infinity
+console.log("not a number" / 2); // NaN, such division is erroneous
+// All mathematical operations are safe
+
+// BigInt
+// “number” type cannot represent integer values larger than (253-1)
+// (that’s 9007199254740991 ), or less than -(253-1) for negatives
+
+// BigInt type was recently added to the language to represent integers of arbitrary length.
+// the "n" at the end means it's a BigInt
+const bigint = 1234567890123456789012345678901234567890n;
+const sameBigint = BigInt("1234567890123456789012345678901234567890");
+const bigintFromNumber = BigInt(10); // same as 10n
+
+// Can be used like a regular number
+console.log(1n + 2n); // 3
+console.log(5n / 2n); // 2
+
+console.log(1n + 2); // Error: Cannot mix BigInt and other types
+
+// number to bigint
+console.log(bigint + BigInt(number)); // 3
+
+// bigint to number
+console.log(Number(bigint) + number); // 3
